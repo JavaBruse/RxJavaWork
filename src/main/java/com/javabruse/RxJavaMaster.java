df0b2b1aa587ba96268092298618fbab56152105
FiataliS <FiataliS@gmail.com>
2025-04-03 11:28:18 +0500

    init all

--- src/main/java/com/javabruse/RxJavaMaster.java
+++ src/main/java/com/javabruse/RxJavaMaster.java
@@ -1,4 +1,29 @@
 package com.javabruse;
 
-public class RxJavaMaster {
+import rx.Observable;
+import rx.Observer;
+
+public class RxJavaMaster  extends Observable implements Observer {
+
+
+    protected RxJavaMaster(OnSubscribe f) {
+        super(f);
+    }
+
+    @Override
+    public void onCompleted() {
+
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+
+    }
+
+    @Override
+    public void onNext(Object object) {
+
+    }
+
 }
+
